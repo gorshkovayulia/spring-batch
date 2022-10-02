3f4d762b4a8276717b5f978e7ad665d7c8254213
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-10-02 21:09:05 +0300

    Add ItemWriter implementation

---
+++ src/main/java/batchsample/ProductJdbcItemWriter.java
@@ -0,0 +1,27 @@
+package batchsample;
+
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class ProductJdbcItemWriter implements ItemWriter<Product> {
+
+    private static final String INSERT_PRODUCT = "insert into product (id,name,description,price) values (?,?,?,?)";
+    private static final String UPDATE_PRODUCT = "update product set name=?, description=?, price=? where id=?";
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductJdbcItemWriter(DataSource ds) {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+
+    public void write(List<? extends Product> items) {
+        for (Product item : items) {// UPSERT, Batch Insert
+            int updated = jdbcTemplate.update(UPDATE_PRODUCT, item.getName(), item.getDescription(), item.getPrice(), item.getId());
+            if (updated == 0) {
+                jdbcTemplate.update(INSERT_PRODUCT, item.getId(), item.getName(), item.getDescription(), item.getPrice());
+            }
+        }
+    }
+}
